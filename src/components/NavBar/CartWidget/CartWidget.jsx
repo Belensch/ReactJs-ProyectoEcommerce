6390774a19df5a28d893f4d979f06da0ce52e982
Belen Schmid <belensch@hotmail.com.ar>
2022-07-13 17:48:37 -0300

    creacion del NavBar, Catwidget con img

---
+++ src/components/NavBar/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import logo from '../../../img/logo.png'
+
+const CartWidget = () => {
+  return (
+    <div>
+      <img src={logo} alt="Logo La cueva de Gollum" className='w-25'/>
+      </div>
+  )
+}
+
+export default CartWidget
